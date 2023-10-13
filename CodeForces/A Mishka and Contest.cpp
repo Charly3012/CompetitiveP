b254495d7cab4f07241e7fda4a87ef9a43b7864f
Carlos Coronado <carlocoronadosilva975@gmail.com>
2023-10-12 23:06:32 -0600

    Mishka and contest resuelto

---
+++ CodeForces/A Mishka and Contest.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+#include <vector>
+
+using namespace std;
+vector<int> frente(vector<int> v, int k);
+vector<int> atras(vector<int> v, int k);
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;         // Número de problemas
+    int k;         // Problem solving skill of kisha
+    vector<int> p; // problemas
+
+    cin >> n >> k;
+
+    for (int i = 0; i < n; i++)
+    {
+        int temp;
+        cin >> temp;
+        p.emplace_back(temp);
+    }
+
+    while (p.empty() == false && p.front() <= k)
+    {
+        p.erase(p.begin());
+    }
+
+    while (p.empty() == false && p.back() <= k)
+    {
+        p.pop_back();
+    }
+
+    n = n - p.size();
+    cout << n << endl;
+
+    return 0;
+}
